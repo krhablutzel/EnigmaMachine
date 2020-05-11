b037341065e3db866968e28df4ab768aca026738
Kathleen Hablutzel <59173829+krhablutzel@users.noreply.github.com>
2020-05-11 11:27:24 -0400

    Updated project file paths using Atom

---
+++ src/test/java/krhablutzel/enigma/MainTest.java
@@ -0,0 +1,11 @@
+package krhablutzel.enigma;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class MainTest {
+  @Test
+  public void testMain() {
+    Assert.assertEquals(true, Main.returnsTrue());
+  }
+}
